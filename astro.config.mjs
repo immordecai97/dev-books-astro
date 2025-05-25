f52f5f7c646eaca3706b67bf878b6c1cc25199e8
Carlos Marquina <carlosscripto@gmail.com>
2025-05-24 21:15:52 -0400

    feat: Add book components and layout for book details
    - Implemented Amazon logo as a component for buy buttons.
    - Created BookScore component to fetch and display book ratings.
    - Developed BuyButton component to handle purchase links based on user location.
    - Added multiple book markdown files with metadata including title, author, description, and purchase links.
    - Configured content collection for books with validation schema.
    - Established a global layout for consistent page structure.
    - Created dynamic routing for individual book pages with static generation.
    - Styled the application using Tailwind CSS for a modern look and feel.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	// En Astro, output: 'server' en astro.config.mjs hace que las páginas se generen dinámicamente en el servidor (SSR)
+	// en cada solicitud, en lugar de crear HTML estático (SSG). 
+	// Sirve para contenido que cambia según el usuario o datos en tiempo real.
+	output: 'server',
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	env: {
+		schema: {
+			SHOW_BUY_BUTTON: envField.boolean({ context: 'server', access: 'public' }), //? nos permite leer la variable de entorne y además tiparla, ya que siempre son cadenas de texto
+			SCORE_API_ENDPOINT: envField.string({ context: 'client', access: 'public' }),
+		}
+	}
+});
